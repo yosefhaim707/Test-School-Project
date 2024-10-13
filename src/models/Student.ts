9508b7aee8d46361bd2b4930c8f67410937bcb5f
Yosef Haim <yosefhaimmalamud@gmail.com>
2024-10-13 12:22:19 +0300

    Added student and teacher schema's. May need some changes

--- src/models/Student.ts
+++ src/models/Student.ts
@@ -1,12 +1,39 @@
 import mongoose, { Schema, Document, Types } from 'mongoose';
 import { IClassRoom } from './ClassRoom';
-import { IGrade } from './Grade';
+import Grade, { IGrade } from './Grade';
+import { type } from 'os';
 
 export interface IStudent extends Document {
     _id: Types.ObjectId;
     name: string;
     email: string;
     password: string;
-    class: IClassRoom['_id'];
+    classRoom: IClassRoom['_id'];
     grades: IGrade[];
 };
+
+const StudentSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    classRoom: {
+        type: Types.ObjectId,
+        ref: 'ClassRoom',
+        required: true
+    },
+    grades: {
+        type: [Grade]
+    }
+});
+
+export default mongoose.model<IStudent>('Student', StudentSchema);
--- src/models/Teacher.ts
+++ src/models/Teacher.ts
@@ -1,4 +1,4 @@
-import mongoose, { Schema, Document, Types } from 'mongoose';
+import mongoose, { Schema, Document, Types, mongo } from 'mongoose';
 
 // Interface of teacher for TypeScript
 export interface ITeacher extends Document {
@@ -6,6 +6,28 @@ export interface ITeacher extends Document {
     name: string;
     email: string;
     password: string;
-    class: Types.ObjectId;
+    classRoom: Types.ObjectId;
 };
 
+const TeacherSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    classRoom: {
+        type: Types.ObjectId,
+        required: true
+    }
+});
+
+export default mongoose.model<ITeacher>('Teacher', TeacherSchema);
